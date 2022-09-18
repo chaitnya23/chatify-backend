9fdf12e9891e538920b05cddeb7e9b0eb020dbd7
chaitnya23 <chaitnyagiri2306@gmail.com>
2022-09-19 00:05:59 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,11 @@ const router = require('express').Router();
 
 const auth_router  = require("../routes/auth.routes");
 const user_router  = require("../routes/user.routes");
-const chat_router  = require("../routes/chat.routes");
+const chat_router  = require("../routes/chat.routes"); 
 
 
 router.use("/api/auth" ,auth_router);
 router.use("/api/user" ,user_router);
-router.use("/api/chat" ,chat_router);
+router.use("/api/chat" ,chat_router); 
 
 module.exports = router;
